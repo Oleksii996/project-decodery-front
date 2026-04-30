76dfff208f393ac42904a17184b1e7037ba15d50
oleksii ponomarenko <alexponomarenko996@gmail.com>
2026-04-30 16:48:38 +0200

    update code

--- components/layout/Header/Header.tsx
+++ components/layout/Header/Header.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import Link from 'next/link';
 import css from './Header.module.css';
 
@@ -6,7 +7,12 @@ export default function Header() {
     <header className={css.header}>
       <div className="container">
         <Link href="/" aria-label="Home">
-          Header
+          <Image
+            src="/img/company_logo_both.svg"
+            alt="Company logo"
+            width={105}
+            height={45}
+          />
         </Link>
       </div>
     </header>
