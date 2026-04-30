03e8ca06692f445cf0dcb574b063f62733d1ba83
Roman Hrydin <romario198901@gmail.com>
2026-04-30 20:55:03 +0300

    addserver routes

--- app/(private)/diary/error.tsx
+++ app/(private)/diary/error.tsx
@@ -1,1 +1,9 @@
-'use client'
+'use client';
+
+interface ErrorProps {
+  error: Error;
+}
+
+export default function Error({ error }: ErrorProps) {
+  return <p>{error.message}</p>;
+}
--- features/diary/components/DiaryPage/DiaryPage.tsx
+++ features/diary/components/DiaryPage/DiaryPage.tsx
@@ -4,14 +4,14 @@ import DiaryList from '../DiaryList/DiaryList';
 import { getAllDiaries } from '../../api';
 
 export default function DiaryPage() {
-  const { data, isLoading, isError } = useQuery({
+  const { data: diaries = [], isLoading, isError } = useQuery({
     queryKey: ['diaries'],
-    queryFn: () => getAllDiaries(),
+    queryFn:  getAllDiaries,
     placeholderData: keepPreviousData,
     refetchOnMount: false,
     retry: false,
   });
-  const diaries = data?.diaries ?? [];
+
   return (
     <section>
       {/* <GreetingBlock/> */}
