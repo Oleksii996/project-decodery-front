0edaa70f8915c3c2241519ad8ac29c8db6ef086d
Sliliia85 <liliyasimbukhova@gmail.com>
2026-05-01 13:13:36 +0200

    inizial work whith UserBar and SideBar

---
+++ components/layout/UserBar/UserBar.tsx
@@ -0,0 +1,49 @@
+
+// "use client";
+// import { useState } from 'react';
+// import { useAuthStore } from '@/store/authStore';
+// import ConfirmationModal from '../ConfirmationModal/ConfirmationModal';
+// import styles from './UserBar.module.css';
+
+// const UserBar = ({ onClose }: { onClose?: () => void }) => {
+//   const { user, logout, isLoading } = useAuthStore();
+//   const [showModal, setShowModal] = useState(false);
+
+//   const handleLogout = async () => {
+//     await logout(); // Запит до бекенду
+//     setShowModal(false);
+//     if (onClose) onClose();
+//   };
+
+//   return (
+//     <div className={styles.wrapper}>
+//       <div className={styles.userInfo}>
+//         <img src={user?.avatar || '/default.png'} alt="Avatar" className={styles.avatar} />
+//         <div className={styles.textData}>
+//           <p className={styles.userName}>{user?.name}</p>
+//           <p className={styles.userEmail}>{user?.email}</p>
+//         </div>
+//       </div>
+
+//       <button 
+//         className={styles.logoutBtn} 
+//         onClick={() => setShowModal(true)}
+//         disabled={isLoading}
+//       >
+//         {isLoading ? <span className={styles.loader}></span> : 'Вихід'}
+//       </button>
+
+//       {showModal && (
+//         <ConfirmationModal
+//           title="Ви впевнені, що хочете вийти?"
+//           confirmButtonText="Вийти"
+//           cancelButtonText="Скасувати"
+//           onConfirm={handleLogout}
+//           onCancel={() => setShowModal(false)}
+//         />
+//       )}
+//     </div>
+//   );
+// };
+
+// export default UserBar;
