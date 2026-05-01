b4e5176a221edc19cc94411d7274e6a5b43e8f81
oleksii ponomarenko <alexponomarenko996@gmail.com>
2026-05-01 18:01:20 +0200

    update code

--- features/onboarding/components/OnboardingPage/OnboardingPage.tsx
+++ features/onboarding/components/OnboardingPage/OnboardingPage.tsx
@@ -11,7 +11,7 @@ export default function OnboardingPage() {
 
       <div className={styles.hero} aria-hidden="true">
         <Image
-          src="/Company Logo.svg"
+          src="/img/onboarding/sprout.png"
           alt=""
           fill
           sizes="(min-width: 1440px) 50vw, 100vw"
