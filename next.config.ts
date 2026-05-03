6903d6b88426fdae3bd6ea8cb0bf1ff035c9be3c
oleksii ponomarenko <alexponomarenko996@gmail.com>
2026-05-03 14:52:29 +0200

    update code

--- next.config.ts
+++ next.config.ts
@@ -1,26 +1,26 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  // без этого не хочет грузить картинки с бека
   images: {
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'ftp.goit.study',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'ac.goit.global',
+        pathname: '/**',
       },
     ],
   },
   reactCompiler: true,
-
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'res.cloudinary.com',
-      },
-    ],
-  },
 };
 
 export default nextConfig;
