4ac59d3b1b81d636bd837f318fffe53981cfdf0f
oleksii ponomarenko <alexponomarenko996@gmail.com>
2026-05-04 19:54:29 +0200

    update code volodymyr greeting

--- components/shared/GreetingBlock/GreetingBlock.tsx
+++ components/shared/GreetingBlock/GreetingBlock.tsx
@@ -1,13 +1,15 @@
 import css from '../GreetingBlock/GreetingBlock.module.css';
-import {useAuthStore} from '@/store/authStore';
+import { useAuthStore } from '@/store/authStore';
 
 export default function GreetingBlock() {
   const authStore = useAuthStore();
-  const {userInfo} = authStore;
+  const { userInfo } = authStore;
 
   return (
     <section className={css.greetingBlock}>
-      <p className={css.title}>{userInfo?.name || 'User'}</p>
+      <p className={css.title}>
+        {'Доброго дня, ' + (userInfo?.name || 'User') + '!'}
+      </p>
     </section>
   );
 }
