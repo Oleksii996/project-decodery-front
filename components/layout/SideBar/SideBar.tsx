63ee9347bab0f11ea3982cb5acee8e89bb3ed113
Vitaliia <vitaliia0907@gmail.com>
2026-05-04 23:34:14 +0200

    fix: journey link route

--- components/layout/SideBar/SideBar.tsx
+++ components/layout/SideBar/SideBar.tsx
@@ -23,7 +23,7 @@ const SideBar = ({ onClose, isOpen }: SideBarProps) => {
 
   const navItems = [
     { label: 'Мій день', href: '/', icon: 'icon-today' },
-    { label: 'Подорож', href: '/journey', icon: 'icon-conversion_path' },
+   { label: 'Подорож', href: '/journey/1', icon: 'icon-conversion_path' },
     { label: 'Щоденник', href: '/diary', icon: 'icon-book' },
     { label: 'Профіль', href: '/profile', icon: 'icon-account_circle' },
   ];
