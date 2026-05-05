f71d0caa7dfd4f8b46e81f379556e90442063c18
oleksii ponomarenko <alexponomarenko996@gmail.com>
2026-05-05 16:26:35 +0200

    update code

--- features/profile/components/ProfilePage/ProfilePage.tsx
+++ features/profile/components/ProfilePage/ProfilePage.tsx
@@ -1,3 +1,14 @@
+'use client';
+
+import ProfileSection from '../ProfileSection/ProfileSection';
+import css from './ProfilePage.module.css';
+
 export default function ProfilePage() {
   return <></>;
+  return (
+    <section className={css.page}>
+      <h1 className={css.title}>Профіль</h1>
+      <ProfileSection />
+    </section>
+  );
 }
