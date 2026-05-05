68c8ca9c2d6352ddfdd5fb77e166c1f5bfcc87fe
Oleksii Ponomarenko <55384068+Oleksii996@users.noreply.github.com>
2026-05-05 21:56:54 +0200

    Delete app/services/tasksApi.ts

--- app/services/tasksApi.ts
+++
@@ -1,7 +0,0 @@
-import axiosInstance from "./axiosInstance";
-
-export const getTasks = () => axiosInstance.get("/tasks");
-export const createTask = (data: { task: string; date: string }) =>
-  axiosInstance.post("/tasks", data);
-export const updateTask = (id: string, data: { task: string; date: string }) =>
-  axiosInstance.patch(`/tasks/${id}`, data);
