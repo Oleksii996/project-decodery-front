fd116a4c46bcc70bed4541488e3f51404bdd69f6
Andrii <zahoruiko.andrii@gmail.com>
2026-05-06 11:33:39 +0200

    done

--- app/api/auth/login/route.ts
+++ app/api/auth/login/route.ts
@@ -13,9 +13,12 @@ export const POST = async (req: NextRequest) => {
     const cookiesStore = await cookies();
     const setCookies = response.headers['set-cookie'];
     if (!setCookies)
-      return NextResponse.json('smth going wrong', {
-        status: 400,
-      });
+      return NextResponse.json(
+        { error: 'No cookies received from server' },
+        {
+          status: 502,
+        }
+      );
 
     const cookieArray = Array.isArray(setCookies) ? setCookies : [setCookies];
 
@@ -24,7 +27,9 @@ export const POST = async (req: NextRequest) => {
       const options = {
         expires: parsedItem.Expires ? new Date(parsedItem.Expires) : undefined,
         path: parsedItem.Path,
-        maxAge: Number(parsedItem['Max-Age']),
+        maxAge: parsedItem['Max-Age']
+          ? Number(parsedItem['Max-Age'])
+          : undefined,
       };
 
       if (parsedItem.refreshToken) {
--- app/api/auth/refresh/route.ts
+++ app/api/auth/refresh/route.ts
@@ -1,62 +1,85 @@
 import { api } from '@/lib/api/api';
+import { isAxiosError } from 'axios';
 import { parse } from 'cookie';
 import { cookies } from 'next/headers';
 import { NextRequest, NextResponse } from 'next/server';
 
 export const POST = async (request: NextRequest) => {
-  const cookisStore = await cookies();
-  const accessToken = cookisStore.get('accessToken');
-  const refreshToken = cookisStore.get('refreshToken');
+  try {
+    const cookiesStore = await cookies();
+    const accessToken = cookiesStore.get('accessToken');
+    const refreshToken = cookiesStore.get('refreshToken');
 
-  if (accessToken) {
+    if (accessToken) {
+      return NextResponse.json({
+        success: true,
+      });
+    }
+
+    if (!refreshToken) {
+      return NextResponse.json(
+        { success: false, error: 'No refresh token' },
+        { status: 401 }
+      );
+    }
+
+    const response = await api.post('api/auth/refresh', null, {
+      headers: {
+        Cookie: cookiesStore.toString(),
+      },
+    });
+
+    const setCookies = response.headers['set-cookie'];
+    if (!setCookies)
+      return NextResponse.json(
+        { success: false, error: 'No cookies received from server' },
+        { status: 502 }
+      );
+
+    const cookieArray = Array.isArray(setCookies) ? setCookies : [setCookies];
+
+    for (const item of cookieArray) {
+      const parsedItem = parse(item);
+      const options = {
+        expires: parsedItem.Expires ? new Date(parsedItem.Expires) : undefined,
+        path: parsedItem.Path,
+        maxAge: parsedItem['Max-Age']
+          ? Number(parsedItem['Max-Age'])
+          : undefined,
+      };
+
+      if (parsedItem.refreshToken) {
+        cookiesStore.set('refreshToken', parsedItem.refreshToken, options);
+      }
+
+      if (parsedItem.accessToken) {
+        cookiesStore.set('accessToken', parsedItem.accessToken, options);
+      }
+
+      if (parsedItem.sessionId) {
+        cookiesStore.set('sessionId', parsedItem.sessionId, options);
+      }
+    }
+
     return NextResponse.json({
       success: true,
+      ...response.data,
     });
-  }
-
-  if (!refreshToken) {
-    return NextResponse.json({
-      success: false,
-    });
-  }
-
-  const response = await api.post('api/auth/refresh', null, {
-    headers: {
-      Cookie: cookisStore.toString(),
-    },
-  });
-
-  const setCookies = response.headers['set-cookie'];
-  if (!setCookies)
-    return NextResponse.json('smth going wrong', {
-      status: 400,
-    });
-
-  const cookieArray = Array.isArray(setCookies) ? setCookies : [setCookies];
-
-  for (const item of cookieArray) {
-    const parsedItem = parse(item);
-    const options = {
-      expires: parsedItem.Expires ? new Date(parsedItem.Expires) : undefined,
-      path: parsedItem.Path,
-      maxAge: Number(parsedItem['Max-Age']),
-    };
-
-    if (parsedItem.refreshToken) {
-      cookisStore.set('refreshToken', parsedItem.refreshToken, options);
+  } catch (error) {
+    if (isAxiosError(error)) {
+      return NextResponse.json(
+        {
+          success: false,
+          error: error.message,
+          response: error.response?.data,
+        },
+        { status: error.response?.status ?? 500 }
+      );
     }
 
-    if (parsedItem.accessToken) {
-      cookisStore.set('accessToken', parsedItem.accessToken, options);
-    }
-
-    if (parsedItem.sessionId) {
-      cookisStore.set('sessionId', parsedItem.sessionId, options);
-    }
+    return NextResponse.json(
+      { success: false, error: 'Internal Server Error' },
+      { status: 500 }
+    );
   }
-
-  return NextResponse.json({
-    success: true,
-    ...response.data,
-  });
 };
