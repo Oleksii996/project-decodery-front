51891e2aeb6d9ad80c84cba6751279b6f40c4ae4
Andrii <zahoruiko.andrii@gmail.com>
2026-05-07 11:18:53 +0200

    done

--- components/layout/AuthBar/AuthBar.tsx
+++ components/layout/AuthBar/AuthBar.tsx
@@ -1,13 +1,15 @@
-"use client";
+'use client';
 
-import Link from "next/link";
-import styles from "./AuthBar.module.css";
+import Link from 'next/link';
+import styles from './AuthBar.module.css';
+import { useAuthStore } from '@/store/authStore';
 
 interface AuthBarProps {
   onClose?: () => void;
 }
 
 const AuthBar = ({ onClose }: AuthBarProps) => {
+  const isAuth = useAuthStore(s => s.isAuth);
   return (
     <div>
       <div className={styles.dividerAuth}></div>
